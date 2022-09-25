343d0696ae17df58369fdfe577348e56d32316ad
Truong Le <truong.le@nuvoair.com>
2022-09-24 22:43:25 -0500

    add jest preset config

--- jest.preset.js
+++ jest.preset.js
@@ -1,3 +1,29 @@
 const nxPreset = require('@nrwl/jest/preset').default;
 
-module.exports = { ...nxPreset };
+module.exports = { 
+    ...nxPreset,
+    transform: {
+        '^(?!.*\\.(js|jsx|ts|tsx|css|json|scss)$)': '@nrwl/react/plugins/jest',
+        '^.+\\.[tj]sx?$': 'ts-jest',
+    },
+    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+    globals: {
+        'ts-jest': {
+        diagnostics: false,
+        tsconfig: '<rootDir>/tsconfig.spec.json',
+        stringifyContentPathRegex: '\\.(html|svg)$',
+        },
+        __webpack_public_path__: 'test',
+    },
+    rootDir: '.',
+    moduleNameMapper: {
+        '\\.(css|sass|scss)$': 'identity-obj-proxy',
+        // replace lodash-es with lodash while unit testing.
+        // Lodash-es is a big module and it takes a lot of time to compile.
+        '^lodash-es$': 'lodash',
+    },
+    testEnvironment: 'jsdom',
+    reporters: ['default'],
+    coverageReporters: ['json'],
+    collectCoverage: true,
+};
--- libs/features/home/jest.config.ts
+++ libs/features/home/jest.config.ts
@@ -3,7 +3,7 @@ export default {
   displayName: 'features-home',
   preset: '../../../jest.preset.js',
   transform: {
-    '^.+\\.[tj]sx?$': 'babel-jest',
+    '^.+\\.[tj]sx?$': 'ts-jest',
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
   coverageDirectory: '../../../coverage/libs/features/home',
